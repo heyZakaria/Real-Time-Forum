0641b6b44b4f862cfff6b08ef72ca3e42944d4ea
soufyaneelasly <elasslsyoufyane@gmail.com>
2025-02-26 19:12:45 +0000

    playing encore

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module lesowebsocket
+module websocket
 
 go 1.22.3
 
--- main.go
+++ main.go
@@ -1,19 +1,66 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	setupAPI()
-	log.Fatal(http.ListenAndServe(":8080",nil))
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
-func setupAPI() {
-	manager:=newmanager()
 
-	
-	http.Handle("/", http.FileServer(http.Dir("./frentend")))
-	http.HandleFunc("/ws", manager.serveWS)
+func treader(conn *websocket.Conn){
+	for {
+		messageType,p,err:=conn.ReadMessage()
+		if err!=nil{
+			log.Println(err)
+			return 
+			
+
+		}
+	log.Println(string(p))
+
+	if err:=conn.WriteMessage(messageType,p);err!=nil{
+		log.Println(err)
+		return
+	}
+
+	}
+
+
+}
+
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "home page ")
+
+
+}
+
+func wsEndPoint(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprint(w, "wsendpoint")
+	upgrader.CheckOrigin=func(r *http.Request) bool {return true }
+
+	ws,err:=upgrader.Upgrade(w,r,nil)
+	if err!=nil{
+		log.Println(err,"Errwsendpoint")
+	}
+	log.Println("CLien succefully...")
+	treader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/ws", wsEndPoint)
+}
+
+func main() {
+	fmt.Println("helo wbsocket")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
